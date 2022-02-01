d6cc861692483bb844731c56c486eb6d6b293edc
jinhee <parkjh6275@naver.com>
2022-02-01 16:25:43 +0900

    별 찍기 - 1

---
+++ Java/src/B_2438.java
@@ -0,0 +1,19 @@
+/*
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제 
+ */
+import java.util.Scanner;
+
+public class B_2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
