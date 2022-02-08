263dd3a7cf820d01668621b42577a014c8e7a3d3
jinhee <parkjh6275@naver.com>
2022-02-08 10:40:00 +0900

    등록

---
+++ Java/src/B_7287.java
@@ -0,0 +1,10 @@
+/*
+ * 자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램 
+ */
+
+public class B_7287 {
+	public static void main(String[] args) {
+		System.out.println("80");
+		System.out.println("parkjh6275");
+	}
+}
