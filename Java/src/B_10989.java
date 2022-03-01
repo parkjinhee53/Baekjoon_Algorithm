a4a78e386173cfdf4da5523c3709fd9c6fe10588
jinhee <parkjh6275@naver.com>
2022-03-01 21:52:12 +0900

    수 정렬하기 3

---
+++ Java/src/B_10989.java
@@ -0,0 +1,30 @@
+/*
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램 
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B_10989 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr); // 배열에 저장된 정수 오름차순으로 정렬 
+		
+		for(int i = 0; i < n; i++) {
+			sb.append(arr[i]).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+
+}
