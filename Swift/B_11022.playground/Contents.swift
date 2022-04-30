468138d861af043e6cd14ef5f4591804e377da7c
jinhee <parkjh6275@naver.com>
2022-04-30 17:13:12 +0900

    A+B - 8

---
+++ Swift/B_11022.playground/Contents.swift
@@ -0,0 +1,10 @@
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램 
+
+let t = Int(readLine()!)! // 테스트 케이스의 개수 입력받기
+
+for i in 1...t{ // 입력받은 테스트 케이스의 개수만큼 반복
+    let arr = readLine()!.split(separator: " ")
+    let a = Int(arr[0])!
+    let b = Int(arr[1])!
+    print("Case #\(i): \(a) + \(b) = \(a+b)")
+}
