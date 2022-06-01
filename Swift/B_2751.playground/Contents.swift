0d4ef987526152553c3e4b95ef5057519e77f76e
jinhee <parkjh6275@naver.com>
2022-06-01 19:45:10 +0900

    수 정렬하기 2

---
+++ Swift/B_2751.playground/Contents.swift
@@ -0,0 +1,14 @@
+// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램
+
+let n = Int(readLine()!)!
+var arr: [Int] = []
+
+for _ in 0..<n {
+    arr.append(Int(readLine()!)!)
+}
+
+arr.sort()
+
+for i in 0..<n {
+    print("\(arr[i])")
+}
