90c763f17dd21b3fb4ca8b103b611703e7ae02a5
parjjinhee53 <parkjh6275@naver.com>
2022-10-25 23:44:04 +0900

    .

--- Swift/B_2530.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-// 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 초 단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램 
-
-var arr = readLine()!.split(separator: " ").map{Int($0)!}
-var h = arr[0]
-var m = arr[1]
-var s = arr[2]
-var d = Int(readLine()!)!
-
-print((h + (m + (s + d)/60)/60) % 24, (m + (s + d) / 60) % 60, (s + d) % 60)
