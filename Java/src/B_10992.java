20bb95c12399e817f44f0d9e17e5d73c6e2d382c
parjjinhee53 <parkjh6275@naver.com>
2023-02-23 15:51:06 +0900

    별 찍기 - 17

---
+++ Java/src/B_10992.java
@@ -0,0 +1,35 @@
+/*
+ * 첫째 줄부터 N번째 줄까지 차례대로 별을 출력하는 프로그램 
+ */
+import java.util.Scanner;
+
+public class B_10992 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for(int i = 1; i <= n; i++) {
+			if (i == n) {
+				for(int k = 1; k <= 2 * i - 1; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+				break;
+			}
+			for(int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 1; k <= 2 * i - 1; k++) {
+				if(k == 1 || k == 2 * i- 1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
